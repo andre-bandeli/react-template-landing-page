cc8d3b975fd02b8476242e7c752d4d50cb1b9604
andre-bandeli <andre.lbandeli@gmail.com>
2022-05-22 17:04:04 -0300

    corrigindo responsivo

--- main/src/components/slider/Slider.jsx
+++ main/src/components/slider/Slider.jsx
@@ -6,10 +6,8 @@ export default function Slider() {
     <div id='slider'>
       <h1>Nossos Clientes</h1>
       <div className="container-md">
-        <div className="col">
-          <h2>
+         <div className="col">
               <img src="assets/images/logo1.svg" alt="" />
-          </h2>
         </div>
         <div className="col">
               <img src="assets/images/logo2.svg" alt="" />
